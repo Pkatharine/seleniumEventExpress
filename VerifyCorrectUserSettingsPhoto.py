8c8e94d9a3ae7e3805e63ef3bcb5d725e9f05e24
katherine <katya.pereverzeva2109@gmail.com>
2019-12-09 20:30:12 +0200

    Initial commit

---
+++ VerifyCorrectUserSettingsPhoto.py
@@ -0,0 +1,51 @@
+import os
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class MyTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path="chromedriver.exe")
+        cls.driver.get("http://localhost:53326/")
+        cls.driver.implicitly_wait(10)
+
+    def test_open_event_express(self):
+        driver = self.driver
+        sign_in = driver.find_element_by_class_name('MuiButton-label').click()
+        email = driver.find_element_by_name("email").send_keys("user@gmail.com")
+        password = driver.find_element_by_name("password")
+        password.send_keys("1qaz1qaz")
+        password.send_keys(Keys.ENTER)
+        username = driver.find_element_by_xpath("//div[@class = 'd-flex flex-column align-items-center']/h4").text
+        self.assertEqual("UserTest", username)
+
+    def test_open_settings_page(self):
+        driver = self.driver
+        home = driver.find_element_by_xpath("//div/a[@href='/profile']/button").click()
+
+    def test_verify_changing_photo(self):
+        driver = self.driver
+        change_photo = driver.find_element_by_xpath("//div[@class = 'MuiExpansionPanelSummary-content']").click()
+        clear_button = driver.find_element_by_xpath("//*[@id='panel1bh-content']/div/p/form/button").click()
+        Imagepath = r"..\seleniumEventExpress\avatar.png"
+        change_photo_input = driver.find_element_by_xpath("//div[@class = 'placeholder-preview ']/input")
+        change_photo_input.send_keys(os.path.abspath(Imagepath))
+        submit_button = driver.find_element_by_xpath("// *[ @ id = 'panel1bh-content'] / div / p / form / div[2] / "
+                                                     "button").click()
+        success = driver.find_element_by_id("client-snackbar").text
+        print(success)
+        self.assertEqual("Avatar is update", success)
+
+        # drv.find_element_by_id("IdOfInputTypeFile").send_keys(os.getcwd()+"/image.png")
+        # change_username_input.send_keys("1")
+        # change_username_input.send_keys(Keys.ENTER)
+        # success = driver.find_element_by_id("client-snackbar").text
+        # self.assertEqual("Username is changed", success)
+
+
+if __name__ == '__main__':
+    unittest.main()
